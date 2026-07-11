94d2de44e785c9eb556231aeab6ec2fec5f6b059
nene23465 <yeremisdejeusberrioprasca@gmail.com>
2026-07-11 16:20:59 -0500

    hice la app de vuelo

--- src/App.java
+++ src/App.java
@@ -1,18 +1,46 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-crea vuelo 5 asientos 
-mostrar info vuelo 
-embarcar asientos A1, A2, A3
-tratar de embarcar A1 otra vez
-mostrar info asientos
-desembarcar A2
-mostrar info asientos
----------------------
-crear 3 vuelos hacer lo mismo con cada vuelo
-diagrama clases
+    public static void main(String[] args) {
+        Vuelo vuelo1 = new Vuelo("AV001", "Bogota", "Medellin", 5);
+        
+        vuelo1.mostrarInfo();
+        vuelo1.embarcar("A1");
+        vuelo1.embarcar("A2");
+        vuelo1.embarcar("A3");
+
+        vuelo1.embarcar("A1");
+        vuelo1.mostrarAsientos();
+        vuelo1.desembarcar("A2");
+
+        vuelo1.mostrarAsientos();
 
 
+        System.out.println("   ");
+
+        Vuelo vuelo2 = new Vuelo("AV002", "Bogota", "Cali", 5);
+        Vuelo vuelo3 = new Vuelo("AV003", "Bogota", "Cartagena", 5);
+        Vuelo vuelo4 = new Vuelo("AV004", "Bogota", "Barranquilla", 5);
 
 
+        Vuelo[] vuelos = {vuelo2, vuelo3, vuelo4};
+
+
+        for (Vuelo vuelo : vuelos) {
+
+            vuelo.mostrarInfo();
+
+            vuelo.embarcar("A1");
+            vuelo.embarcar("A2");
+            vuelo.embarcar("A3");
+
+            vuelo.embarcar("A1");
+
+            vuelo.mostrarAsientos();
+
+            vuelo.desembarcar("A2");
+
+            vuelo.mostrarAsientos();
+
+            System.out.println("");
+        }
     }
 }
