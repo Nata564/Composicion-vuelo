c78eab2fe8ea1b5ee183a78eecbab1d0b1e9c244
Nata564 <lizpp37@gmail.com>
2026-07-15 08:49:22 -0500

    update app-vuelo

--- src/App.java
+++ src/App.java
@@ -2,19 +2,23 @@ public class App {
     public static void main(String[] args) {
         Vuelo vuelo1 = new Vuelo("AV001", "Bogota", "Medellin", 5);
         
+        System.out.println("----Informacion del vuelo----");
         vuelo1.mostrarInfo();
+System.out.println("----Embarque---------");        
         vuelo1.embarcar("A1");
         vuelo1.embarcar("A2");
         vuelo1.embarcar("A3");
-
+System.out.println("--------------");
         vuelo1.embarcar("A1");
+System.out.println("-------informacion asientos-------");
         vuelo1.mostrarAsientos();
+        System.out.println("--------------");
         vuelo1.desembarcar("A2");
-
+System.out.println("-------informacion asientos-------");
         vuelo1.mostrarAsientos();
 
 
-        System.out.println("   ");
+        System.out.println("----VUELOS------");
 
         Vuelo vuelo2 = new Vuelo("AV002", "Bogota", "Cali", 5);
         Vuelo vuelo3 = new Vuelo("AV003", "Bogota", "Cartagena", 5);
@@ -25,22 +29,20 @@ public class App {
 
 
         for (Vuelo vuelo : vuelos) {
-
+System.out.println("----Informacion del vuelo----");
             vuelo.mostrarInfo();
-
+System.out.println("----Embarque---------"); 
             vuelo.embarcar("A1");
             vuelo.embarcar("A2");
             vuelo.embarcar("A3");
-
+System.out.println("--------------");
             vuelo.embarcar("A1");
-
+System.out.println("-------informacion asientos-------");
             vuelo.mostrarAsientos();
-
+System.out.println("--------------");
             vuelo.desembarcar("A2");
-
+System.out.println("-------informacion asientos-------");
             vuelo.mostrarAsientos();
-
-            System.out.println("");
         }
     }
 }
